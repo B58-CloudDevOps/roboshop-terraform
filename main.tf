b7bdadec91e553d26a8c3f0d2401a3327bc8e350
CodingManoj <CodingManoj@gmail.com>
2024-12-02 20:42:48 -0500

    Expense Infra

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "name" {
-  count         = length(var.components)
-  ami           = data.aws_ami.main.image_id
-  instance_type = "t3.small"
+  count                  = length(var.components)
+  ami                    = data.aws_ami.main.image_id
+  instance_type          = "t3.small"
   vpc_security_group_ids = ["aws_security_group.main.*.id[count.index]"]
 
   tags = {
-    Name = "${var.components[count.index]-${var.env}}"
+    Name = "${var.components[count.index]}-${var.env}"
   }
 }
 
