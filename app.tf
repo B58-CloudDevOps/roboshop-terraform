a2bc129b2761d3bb3b6bffec56b1f6376abdf782
CodingManoj <CodingManoj@gmail.com>
2024-12-10 19:51:43 -0500

    Dry Code

--- app.tf
+++ app.tf
@@ -6,14 +6,15 @@ resource "null_resource" "app" {
   triggers = {
     always_run = timestamp()
   }
-
-  connection {
-    host     = aws_instance.main.*.private_ip[count.index]
-    user     = data.vault_generic_secret.ssh.data["username"]
-    password = data.vault_generic_secret.ssh.data["password"]
-    type     = "ssh"
-  }
   provisioner "remote-exec" { # This let's the execution to happen on the remote node
+
+    connection {
+      host     = aws_instance.main.*.private_ip[count.index]
+      user     = data.vault_generic_secret.ssh.data["username"]
+      password = data.vault_generic_secret.ssh.data["password"]
+      type     = "ssh"
+    }
+
     inline = [
       "sudo pip3.11 install ansible",
       "ansible-pull -U https://github.com/B58-CloudDevOps/roboshop-ansible.git -i localhost, -e component=${component} -e env=${env} -e vault_token=${vault_token} main.yml"
