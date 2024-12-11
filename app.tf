675b188e7b0a3cdc9d56155f8ab92095a869da78
CodingManoj <CodingManoj@gmail.com>
2024-12-10 19:54:32 -0500

    Vault Integration

--- app.tf
+++ app.tf
@@ -7,17 +7,15 @@ resource "null_resource" "app" {
     always_run = timestamp()
   }
   provisioner "remote-exec" { # This let's the execution to happen on the remote node
-
     connection {
       host     = aws_instance.main.*.private_ip[count.index]
       user     = data.vault_generic_secret.ssh.data["username"]
       password = data.vault_generic_secret.ssh.data["password"]
-      type     = "ssh"
     }
 
     inline = [
       "sudo pip3.11 install ansible",
-      "ansible-pull -U https://github.com/B58-CloudDevOps/roboshop-ansible.git -i localhost, -e component=${component} -e env=${env} -e vault_token=${vault_token} main.yml"
+      "ansible-pull  -i localhost, -U https://github.com/B58-CloudDevOps/roboshop-ansible.git -e component=${var.components[count.index]} -e env=${var.env} -e vault_token=${var.vault_token} main.yml"
     ]
   }
 }
