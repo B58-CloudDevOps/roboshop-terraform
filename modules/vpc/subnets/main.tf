049aaaea9c19ee0eac991cd9fe145c848ed258e8
CodingManoj <CodingManoj@gmail.com>
2024-12-11 20:43:16 -0500

    Refactoring the tf code along with modules for vpc

--- modules/vpc/subnets/main.tf
+++ modules/vpc/subnets/main.tf
@@ -42,7 +42,7 @@ resource "aws_route" "igw_route" {
 
   route_table_id         = aws_route_table.main.*.id[count.index]
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
+  gateway_id             = aws_internet_gateway.igw.*.id[0]
 }
 
 
