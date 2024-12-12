79ce768358f2cb244c2dcf3568226e4a6bfc6124
CodingManoj <CodingManoj@gmail.com>
2024-12-11 20:05:08 -0500

    Refactoring the tf code along with modules for vpc

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,16 +5,15 @@ resource "aws_vpc" "main" {
   tags = {
     Name = "${var.env}-vpc"
   }
-
 }
 
 module "subnets" {
-  source   = "./subnets"
-  for_each = var.subnets
+  source = "./subnets"
 
-  name              = each.key
-  cidr              = each.value["cidr"]
-  availabilty_zones = var.availabilty_zones
-  vpc_id            = aws_vpc.main.id
-  env               = var.env
+  for_each           = var.subnets
+  name               = each.key
+  availability_zones = var.availabilty_zones
+  vpc_id             = aws_vpc.main.id
+  cidr               = each.value["cidr"]
+  env                = var.env
 }
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -2,3 +2,4 @@ variable "cidr" {}
 variable "env" {}
 variable "subnets" {}
 variable "availabilty_zones" {}
+variable "name" {}
