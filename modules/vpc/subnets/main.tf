6c2dd6742d978d362b786dc5fd0ab38111332f91
CodingManoj <CodingManoj@gmail.com>
2024-12-11 20:26:28 -0500

    Refactoring the tf code along with modules for vpc

--- modules/vpc/subnets/main.tf
+++ modules/vpc/subnets/main.tf
@@ -19,3 +19,9 @@ resource "aws_route_table" "main" {
     Name = "${var.name}-${var.env}-${split("-", var.availability_zones[count.index])[2]}"
   }
 }
+
+resource "aws_main_route_table_association" "main" {
+  count          = length(var.cidr)
+  vpc_id         = var.vpc_id
+  route_table_id = aws_route_table.main.*.id[count.index]
+}
