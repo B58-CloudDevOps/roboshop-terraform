44ebd6c2cdbddb43c3c0bb9c15b01dfacbf469f5
CodingManoj <CodingManoj@gmail.com>
2024-12-15 20:04:19 -0500

    Refactoring the tf code along with modules for vpc

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -53,7 +53,7 @@ resource "aws_vpc_peering_connection" "main" {
 
 resource "aws_route" "on_peer_side" {
   for_each                  = var.peering_vpcs
-  route_table_id            = each.value[route_table_id]
+  route_table_id            = each.value["route_table_id"]
   destination_cidr_block    = var.cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.main[each.key].id
 }
