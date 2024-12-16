e51d171cd1c82aca158a65bd83818f0a5319d0db
CodingManoj <CodingManoj@gmail.com>
2024-12-15 20:43:35 -0500

    Refactoring the tf code along with modules for vpc

--- modules/vpc/subnets/main.tf
+++ modules/vpc/subnets/main.tf
@@ -53,9 +53,9 @@ resource "aws_route" "ngw_route" {
   nat_gateway_id         = var.ngw_ids[count.index]
 }
 
-# resource "aws_route" "all_route" {
-#   count                     = length(var.cidr)
-#   route_table_id            = aws_route_table.main.*.id[count.index]
-#   destination_cidr_block    = "172.31.0.0/16" # Default VPC Cidr 
-#   vpc_peering_connection_id = var.vpc_peering_ids
-# }
+resource "aws_route" "all_route" {
+  count                     = length(var.cidr)
+  route_table_id            = aws_route_table.main.*.id[count.index]
+  destination_cidr_block    = "172.31.0.0/16" # Default VPC Cidr 
+  vpc_peering_connection_id = var.vpc_peering_ids
+}
