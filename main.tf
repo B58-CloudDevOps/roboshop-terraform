7b94639477b7162f66ab581e52fce499fc2ad930
CodingManoj <CodingManoj@gmail.com>
2024-12-15 22:06:43 -0500

    Refactoring the tf code along with modules for vpc

--- main.tf
+++ main.tf
@@ -12,11 +12,11 @@ module "vpc" {
   def_vpc_cidr       = var.def_vpc_cidr
 }
 
-module "db" {
-  for_each = var.db_servers
+# module "db" {
+#   for_each = var.db_servers
 
-  source        = "./modules/ec2"
-  env           = var.env
-  ports         = each.value["ports"]
-  instance_type = each.value[instance_type]
-}
+#   source        = "./modules/ec2"
+#   env           = var.env
+#   ports         = each.value["ports"]
+#   instance_type = each.value[instance_type]
+# }
