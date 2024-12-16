a71daa8e7a70913cf10f13c0bf0d47dd59d59af9
CodingManoj <CodingManoj@gmail.com>
2024-12-15 19:56:25 -0500

    Refactoring the tf code along with modules for vpc

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -45,4 +45,8 @@ resource "aws_vpc_peering_connection" "main" {
   peer_vpc_id = each.value["id"]
   vpc_id      = aws_vpc.main.id
   auto_accept = true
+
+  tags = {
+    Name = "${each.key}-peer"
+  }
 }
