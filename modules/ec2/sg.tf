f3c9cbecb20204417ce0c238ab04f763b2b2870e
CodingManoj <CodingManoj@gmail.com>
2024-12-16 20:11:13 -0500

    Refactoring the tf code along with modules for vpc

--- modules/ec2/sg.tf
+++ modules/ec2/sg.tf
@@ -3,6 +3,18 @@ resource "aws_security_group" "main" {
   description = "${var.component_name}-${var.env}-sg"
   vpc_id      = var.vpc_id
 
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   tags = {
     Name = "${var.component_name}-${var.env}-sg"
   }
