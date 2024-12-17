0b3733b4f5c48310d3ebf71d5be1612f830e19b4
CodingManoj <CodingManoj@gmail.com>
2024-12-17 18:20:41 -0500

    Refactoring the tf code along with modules for vpc

--- main.tf
+++ main.tf
@@ -32,24 +32,24 @@ module "db" {
   subnet_ids   = module.vpc["main"].subnets["db"].subnets
 }
 
-module "app" {
-  depends_on = [module.db]
+# module "app" {
+#   depends_on = [module.db]
 
-  source   = "./modules/ec2"
-  for_each = var.app_servers
+#   source   = "./modules/ec2"
+#   for_each = var.app_servers
 
-  component_name = each.key
+#   component_name = each.key
 
-  env           = var.env
-  vault_token   = var.vault_token
-  ports         = each.value["ports"]
-  instance_type = each.value["instance_type"]
+#   env           = var.env
+#   vault_token   = var.vault_token
+#   ports         = each.value["ports"]
+#   instance_type = each.value["instance_type"]
 
-  vpc_id       = module.vpc["main"].vpc_id
-  zone_id      = var.zone_id
-  bastion_host = var.bastion_host
-  subnet_ids   = module.vpc["main"].subnets["app"].subnets
-}
+#   vpc_id       = module.vpc["main"].vpc_id
+#   zone_id      = var.zone_id
+#   bastion_host = var.bastion_host
+#   subnet_ids   = module.vpc["main"].subnets["app"].subnets
+# }
 
 
 module "web" {
