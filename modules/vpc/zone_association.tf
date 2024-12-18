1c57be8fad9309cf00cbc027caa0bc816382b5e7
CodingManoj <CodingManoj@gmail.com>
2024-12-17 19:57:28 -0500

    Refactoring the tf code along with modules for vpc

--- modules/vpc/zone_association.tf
+++ modules/vpc/zone_association.tf
@@ -1,4 +1,11 @@
-resource "aws_route53_zone_association" "main" {
-  zone_id = var.zone_id
-  vpc_id  = aws_vpc.main.id
+# resource "aws_route53_zone_association" "main" {
+#   zone_id = var.zone_id
+#   vpc_id  = aws_vpc.main.id
+# }
+
+resource "aws_route53_zone" "private" {
+  name = "roboshop.internal"
+  vpc {
+    vpc_id = aws_vpc.main.id
+  }
 }
