cf61e96107713290433e20c2aab7ec4b3df47f66
CodingManoj <CodingManoj@gmail.com>
2024-12-18 23:07:23 -0500

    Provisioning ALB and SG

--- modules/ec2/data.tf
+++ modules/ec2/data.tf
@@ -2,6 +2,12 @@ data "aws_ami" "main" {
   most_recent = true
   name_regex  = "b58-golden-image"
   owners      = ["355449129696"]
+  filters = [
+    {
+      name   = "tag:Version"
+      values = ["1.0.0"] # Use a specific version tag
+    }
+  ]
 }
 
 data "vault_generic_secret" "ssh" {
