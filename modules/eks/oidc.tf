7e9a2950bb124f35728eb47c6686d18b2c45e0e3
CodingManoj <CodingManoj@gmail.com>
2024-12-22 23:57:47 -0500

    Deploys Nginx Ingress Controller with custom values

---
+++ modules/eks/oidc.tf
@@ -0,0 +1,12 @@
+resource "aws_iam_openid_connect_provider" "main" {
+  url = aws_eks_cluster.main.identity.oidc[0].issuer
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+  thumbprint_list = [data.external.oidc_thumbprint.result.thumbprint]
+}
+
+data "external" "oidc_thumbprint" {
+  program = ["kubergrunt", "eks", "oidc-thumbprint", "--issuer-url", aws_eks_cluster.main.identity.oidc[0].issuer]
+}
