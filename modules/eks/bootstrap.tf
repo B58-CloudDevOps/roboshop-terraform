018b8fbb0c77df5c4ea583057592e13091f33a32
CodingManoj <CodingManoj@gmail.com>
2024-12-22 20:43:36 -0500

    Deploys Nginx Ingress Controller

---
+++ modules/eks/bootstrap.tf
@@ -0,0 +1,13 @@
+# Deploys Nginx Ingress Controller In kube-system namespace
+resource "null_resource" "nginx_ingress" {
+
+  depends_on = [aws_eks_cluster.main, aws_eks_node_group.main]
+
+  provisioner "local-exec" {
+    command = <<EOF
+
+aws eks update-kubeconfig --name "${var.env}-eks"
+helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace kube-system
+EOF
+  }
+}
