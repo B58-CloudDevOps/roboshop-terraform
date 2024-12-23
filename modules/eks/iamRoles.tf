9f388b667cda99e6041ad6875f9d6896bba1c912
CodingManoj <CodingManoj@gmail.com>
2024-12-22 20:15:36 -0500

    Provisioning EKS - Adding IAM Roles

--- modules/eks/iamRoles.tf
+++ modules/eks/iamRoles.tf
@@ -6,7 +6,7 @@ resource "aws_iam_role" "eks_role" {
       Action = "sts:AssumeRole"
       Effect = "Allow"
       Principal = {
-        Service = "ek2.amazonaws.com"
+        Service = "eks.amazonaws.com"
       }
     }]
     Version = "2012-10-17"
