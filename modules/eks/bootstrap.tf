0e514964eb6f21043d9c2997342aa57f2239cff8
CodingManoj <CodingManoj@gmail.com>
2024-12-22 23:38:17 -0500

    Deploys Nginx Ingress Controller with custom values

--- modules/eks/bootstrap.tf
+++ modules/eks/bootstrap.tf
@@ -1,5 +1,19 @@
 # Deploys Nginx Ingress Controller In kube-system namespace
 resource "null_resource" "nginxIngress" {
+
+  depends_on = [aws_eks_cluster.main, aws_eks_node_group.main]
+
+  provisioner "local-exec" {
+    command = <<EOF
+
+aws eks update-kubeconfig --name "${var.env}-eks"
+helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace kube-system -f ${path.module}/ingressValues.yaml
+EOF
+  }
+}
+
+
+resource "null_resource" "externalDns" {
   triggers = {
     always = timestamp()
   }
@@ -9,7 +23,8 @@ resource "null_resource" "nginxIngress" {
     command = <<EOF
 
 aws eks update-kubeconfig --name "${var.env}-eks"
-helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace kube-system -f ${path.module}/ingressValues.yaml
+helm repo add external-dns https://kubernetes-sigs.github.io/external-dns/
+helm upgrade --install external-dns external-dns/external-dns --version 1.15.0 --namespace kube-system
 EOF
   }
 }
