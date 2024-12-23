72391e58d785f95a9fef306d1f47307b2da2b7f4
CodingManoj <CodingManoj@gmail.com>
2024-12-22 20:52:47 -0500

    Deploys Nginx Ingress Controller with custom values

--- modules/eks/bootstrap.tf
+++ modules/eks/bootstrap.tf
@@ -1,6 +1,8 @@
 # Deploys Nginx Ingress Controller In kube-system namespace
 resource "null_resource" "nginx_ingress" {
-
+  triggers = {
+    always = timestamp()
+  }
   depends_on = [aws_eks_cluster.main, aws_eks_node_group.main]
 
   provisioner "local-exec" {
