7738232dfb51f0522db92202fea52ec96a6efcb4
CodingManoj <CodingManoj@gmail.com>
2024-12-23 20:31:07 -0500

    Provisions OIDC Provider

--- modules/eks/data.tf
+++ modules/eks/data.tf
@@ -1,3 +1,5 @@
 data "external" "oidc_thumbprint" {
   program = ["kubergrunt", "eks", "oidc-thumbprint", "--issuer-url", aws_eks_cluster.main.identity[0].oidc[0].issuer]
 }
+
+data "aws_caller_identity" "current" {}
