eb4606c54dd1ce87fcf001376809a5c791f23748
CodingManoj <CodingManoj@gmail.com>
2025-01-05 21:51:18 -0500

    Schema Injection To MongoDB & mySQL using K8 Jobs

--- modules/eks/bootstrap.tf
+++ modules/eks/bootstrap.tf
@@ -84,9 +84,6 @@ resource "helm_release" "fluentd" {
 resource "null_resource" "argo_cd" {
   depends_on = [aws_eks_cluster.main, aws_eks_node_group.main, null_resource.nginxIngress, null_resource.externalDns]
 
-triggers = always {
-    timestamp = timestamp()
-  }
   provisioner "local-exec" {
     command = <<EOF
 aws eks update-kubeconfig --name "${var.env}-eks"
